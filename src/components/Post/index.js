d46fefb379dc424a2fbf82d36b12476d697b25e8
DaWenXi <zengxin@bianfeng.com>
2022-05-13 13:16:10 +0800

    feat: add right&bottom container and text

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import Video from 'react-native-video';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
 import style from './style';
-import testVideo from './test.mp4';
+import testVideo from '../../assets/test.mp4';
+import avatarImg from '../../assets/avatar.jpeg';
 
 const Post = () => {
   const [pause, setPause] = useState(false);
@@ -14,14 +19,43 @@ const Post = () => {
   return (
     <View style={style.container}>
       <TouchableWithoutFeedback onPress={handlePress}>
-        <Video
-          style={style.video}
-          source={testVideo}
-          resizeMode='cover'
-          paused={pause}
-          repeat
-          onError={e => console.log('video error', e)}
-        />
+        <View>
+          <Video
+            style={style.video}
+            source={testVideo}
+            resizeMode='cover'
+            paused={pause}
+            repeat
+            onError={e => console.log('video error', e)}
+          />
+
+          <View style={style.rightContainer}>
+            <View>
+              <Image source={avatarImg} style={style.avatarContainer} />
+            </View>
+
+            <View style={style.iconContainer}>
+              <AntDesign name={'heart'} size={40} color='white' />
+              <Text style={style.textUnderIcon}>12.1M</Text>
+            </View>
+
+            <View style={style.iconContainer}>
+              <FontAwesome name={'commenting'} size={40} color='white' />
+              <Text style={style.textUnderIcon}>12.1M</Text>
+            </View>
+
+            <View style={style.iconContainer}>
+              <Fontisto name={'share-a'} size={35} color='white' />
+              <Text style={style.textUnderIcon}>121.1k</Text>
+            </View>
+          </View>
+
+          <View style={style.bottomContainer}>
+            <Text style={style.bottomText}>@james_bond</Text>
+            <Text style={style.bottomText}>hhhjhkhkh@james_bond</Text>
+            <Text style={style.bottomText}>llkklklklklkl</Text>
+          </View>
+        </View>
       </TouchableWithoutFeedback>
     </View>
   );
--- src/components/Post/style.js
+++ src/components/Post/style.js
@@ -4,11 +4,49 @@ const style = StyleSheet.create({
   container: {
     width: '100%',
     height: Dimensions.get('window').height,
-    backgroundColor: '#aaa'
+    backgroundColor: '#aaa',
+    position: 'relative'
   },
   video: {
     width: '100%',
     height: '100%'
+  },
+  // rightContainer 相关样式
+  rightContainer: {
+    position: 'absolute',
+    right: 0,
+    bottom: 200,
+    alignItems: 'center'
+  },
+  avatarContainer: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#fff',
+    marginBottom: 28
+  },
+  iconContainer: {
+    marginBottom: 18
+  },
+  textUnderIcon: {
+    color: '#fff',
+    marginTop: 6
+  },
+
+  // bottomContainer 相关样式
+
+  bottomContainer: {
+    width: '100%',
+    height: 100,
+    position: 'absolute',
+    bottom: 70,
+    left: 20
+  },
+  bottomText: {
+    color: '#fff',
+    fontSize: 17,
+    marginBottom: 18
   }
 });
 
